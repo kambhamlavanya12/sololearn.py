c264d0f85ab98d384d2e67d23e8669e13441cf6c
K Lavanya Reddy <117257210+kambhamlavanya12@users.noreply.github.com>
2023-01-14 19:14:34 +0530

    Add files via upload

---
+++ if_else_and_elif.py
@@ -0,0 +1,29 @@
+# if 1 + 1 == 2:
+#    if 2 * 2 == 8:
+#       print("if")
+#    else:
+#       print("else")
+
+# num = 3
+# if num == 1:
+#   print("One")
+# else: 
+#   if num == 2:
+#     print("Two")
+#   else: 
+#     if num == 3: 
+#       print("Three")
+#     else: 
+#       print("Something else")
+
+'''For too many if/else statements use "elif"  '''
+
+# num = 3
+# if num == 1:
+#   print("One")
+# elif num == 2:
+#   print("Two")
+# elif num == 3: 
+#   print("Three")
+# else: 
+#   print("Something else")
